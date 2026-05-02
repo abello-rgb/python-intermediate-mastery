09f4f6e679c275e32f39686406af58197b713570
Breiner Abello <breiner.abello@cun.edu.co>
2026-05-02 08:33:17 -0500

    Add artifact information and display functionality in data structures example

--- Data_estructures/01-Data_estructures.py
+++ Data_estructures/01-Data_estructures.py
@@ -1,1 +1,22 @@
-print('Hi')
+
+artefac_info = {
+    'artist':'Louis C. Tiffany',
+    'period':'ca',
+    'date':194
+}
+
+
+print(artefac_info)
+print('Keys:', artefac_info.keys())
+print('Values:', artefac_info.values())
+
+
+
+for key in artefac_info.keys():
+    print(key)
+
+for value in artefac_info.values():
+    print(value)
+
+for key, value in artefac_info.items():
+    print('Key:', key, 'Value:', value)
